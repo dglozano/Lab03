16e0f1b2236d55dd635a1c0733b7872ac251e7b3
dglozano <diegogarcialozano95@gmail.com>
2017-09-19 21:13:29 -0300

    Crea layout oferta y adapter
    Crea el layout de oferta de trabajo, la clase adapter y copiamos
    los recursos necesarios. Falta implementar el adapter y
    retocar el layout.

---
+++ app/src/main/java/gruporebechi_garcialozano_tibalt/dam/isi/frsf/lab03/OfertasListAdapter.java
@@ -0,0 +1,40 @@
+package gruporebechi_garcialozano_tibalt.dam.isi.frsf.lab03;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by diegogarcialozano on 19/09/17.
+ */
+
+public class OfertasListAdapter extends BaseAdapter {
+
+    LayoutInflater inflater;
+
+    public OfertasListAdapter(Context applicationContext) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
