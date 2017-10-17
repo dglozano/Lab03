64afba8ccf66e53f8494fa723dc8b27e726350de
dglozano <diegogarcialozano95@gmail.com>
2017-10-17 20:53:21 -0300

    Lanza Notificacion al postularse a oferta y borra al descartar
    Crea una nueva clase Receiver que es encargada de recibir el broadcast
    y luego lanza la notificiacion. Debieron agregarse tanto el Intent como la clase
    Receiver al manifest.
    
    Ademas, de seleccionar "Descartar" en el ContextMenu, borra de la lista de ofertas
    al trabajo que fue seleccionado.

---
+++ app/src/main/java/gruporebechi_garcialozano_tibalt/dam/isi/frsf/lab03/NotificationReceiver.java
@@ -0,0 +1,10 @@
+package gruporebechi_garcialozano_tibalt.dam.isi.frsf.lab03;
+
+import android.content.BroadcastReceiver;
+
+/**
+ * Created by diegogarcialozano on 17/10/17.
+ */
+
+public class NotificationReceiver extends BroadcastReceiver {
+}
